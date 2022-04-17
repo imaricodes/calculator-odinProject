45fbade9d92bc10171144f7255595f9fb9878a95
Imari Childress <imaricodes@gmail.com>
2022-04-17 10:54:13 -0400

    Basic working calculator

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//operator functions
 const addCalc = (num1, num2) => num1 + num2;
 
 const multiplyCalc = (num1, num2) => num1 * num2;
@@ -5,8 +6,9 @@ const multiplyCalc = (num1, num2) => num1 * num2;
 let num1;
 let num2;
 let operator;
-
 let sum;
+let result = false;
+let test;
 
 
 
@@ -14,14 +16,35 @@ let numberSelection = document.querySelectorAll('.num-btn');
 
 let operatorSelection = document.querySelectorAll('.operator-btn');
 
+let equalsSelection = document.querySelector('.equals-btn');
+
+
+
 numberSelection.forEach((button) => {
     button.addEventListener('click', (e)=> {
-        num1 = parseInt(e.target.innerText);
-        //console.log(typeof (num1));
-        console.log(`num 1 = ${num1}`);
-        // console.log(e.target.id);
+            //set num1
+            if (num1 == undefined && result == false) {  
+                num1 = e.target.innerText;
+                console.log("num1: " + num1);
+            }
+            else if (!operator && result == false){
+                num1 += e.target.innerText;
+                console.log(num1);
+            }
+            //set num2
+            else if (operator && result == false) {
+                if (num2 == undefined) {
+                    num2 = e.target.innerText;
+                }
+                else if (num2!== undefined && result == false) {
+                    num2 += e.target.innerText;
+                }
+                
+                console.log("num2: " + num2);
+            }
     })
 });
+
 
 operatorSelection.forEach((button) => {
     button.addEventListener('click', (e)=> {
@@ -31,12 +54,9 @@ operatorSelection.forEach((button) => {
 });
 
 
-console.log(numberSelection);
-
-
 let operate = function operation (num1, num2, operator){
-    if (operator === "add") {
-        sum = addCalc(num1, num2);
+    if (operator == "add") {
+        sum = addCalc(parseInt(num1), parseInt(num2));
         return sum; 
     }
     else if (operator === "multiply") {
@@ -45,10 +65,16 @@ let operate = function operation (num1, num2, operator){
     }
 }
 
+equalsSelection.addEventListener('click', function (){
+    
+    let solution = operate(num1, num2, operator);
+    console.log(solution);
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    result = false;
+    console.log("sum: " +sum);
+});
 
 
-
-// console.log(operate(num1, num2, operator));
-// console.log(sum+= 2);
-// console.log(sum);
-
+console.log(numberSelection);
